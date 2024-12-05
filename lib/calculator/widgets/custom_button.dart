7a1c0249f1cf7f84e7dabb78db4b69416ecd36f2
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-04 19:26:02 -0500

    refactor(calculator): change custom button properties.

--- lib/calculator/widgets/custom_button.dart
+++ lib/calculator/widgets/custom_button.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+/// Custom button widget
+/// 
+/// This widget is used to create custom buttons for the calculator
+/// 
+/// Gonzalo Quedena
 class CustomButton extends StatelessWidget {
+
+  /// Button label
   final String label;
-  final VoidCallback? onPressed;
+  /// Button background color
+  final int bgColor;
+  /// Button text size
+  final double textSize;
+  /// Button onPressed function
+  final Function onPressed;
 
-  const CustomButton({super.key, required this.label, required this.onPressed});
+  /// Constructor
+  /// 
+  /// The constructor requires the following parameters:
+  const CustomButton({
+    super.key,
+    this.textSize = 20,
+    required this.label,
+    this.bgColor = 0xFF21252B,
+    required this.onPressed,
+  });
 
+  /// Build method
+  /// 
+  /// This method is used to build the widget for the button
+  /// 
+  /// params [BuildContext] context - The context of the widget
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      enableFeedback: true,
-      elevation: 5,
-      shape: const StadiumBorder(),
-      onPressed: onPressed,
-      child: Text(
-        label,
-        style: const TextStyle(fontSize: 24),
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: SizedBox(
+        width: 65,
+        height: 65,
+        child: TextButton(
+          onPressed: () => {onPressed(label)},
+          style: TextButton.styleFrom(
+            backgroundColor: Color(bgColor),
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+          child: Text(
+            label,
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(fontSize: textSize),
+            ),
+          ),
+        ),
       ),
     );
   }
